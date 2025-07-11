3bece9bbe18cc70378f3814b35fe2ed2c003f187
Andrew Liu <andrewliu339@gmail.com>
2025-07-10 23:45:01 -0400

    added backend

--- part3/3.1/index.js
+++ part3/3.1/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const Note = require('./models/note')
+
 const app = express()
 var morgan = require('morgan')
 const cors = require('cors')        // <-- Add this line
+const PORT = process.env.PORT
 
 app.use(express.json())             // Needed to parse JSON bodies
 app.use(cors())                     // <-- And this line
@@ -15,57 +18,36 @@ morgan.token('body', (req) => {
 // Use morgan with custom format including :body token
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons', (req, res) =>  {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/info', (req, res) => {
-    const count = persons.length
-    const date = new Date()
-    res.send(
-        `<p>Phonebook has info for ${count} people</p>
-        <p>${date}</p>`
-    )
+    Person.find({}).then(persons => {
+        res.send(
+            `<p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date()}</p>`
+        )
+    })
 })
 
 app.get('/info/:id', (req, res) => {
-    const id = req.params.id
-    const person = person.find(person => person.id === id)
-    if(person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Note.findById(req.params.id).then(note => {
+        if (note) {
+            res.json(note)
+        } else {
+            res.status(404).end()
+        }
+    })
 })
 
 app.delete('api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
 })
 
 app.post('/api/persons', (req, res) => {
@@ -86,17 +68,17 @@ app.post('/api/persons', (req, res) => {
     }
 
     const id = Math.floor(Math.random() * 1000000)
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
         id: String(id)
-    }
+    })
 
-    persons = persons.concat(person)
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 10000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ part3/3.1/mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const db_password = process.argv[2]
+const db_username = 'andrewruler'
+const url = `mongodb+srv://${db_username}:${db_password}@cluster0.i5jrafj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    const personSchema = new mongoose.Schema({
+      name: String,
+      number: String,
+    })
+
+    const Person = mongoose.model('Person', personSchema)
+
+    if(process.argv.length > 3) {
+      const name = process.argv[4]
+      const number = process.argv[5]
+
+      const person = Person({name, number})
+      person.save()
+      .then(() => {
+        console.log(`saved ${person}`)
+      })
+      .catch(err => {
+        console.log('Error saving person:', err)
+      })
+    }
+    Person.find({})
+      .then(result => {
+        if (result.length === 0) {
+          console.log('No persons found in the database.')
+        } else {
+          result.forEach(note => {
+            console.log(note)
+          })
+        }
+        mongoose.connection.close()
+      })
+      .catch(err => {
+        console.error('Error querying persons:', err)
+        mongoose.connection.close()
+      })
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err)
+  })
